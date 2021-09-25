9c838d9c67ac4d4abfd6113296f82783ae5aeee8
stoyanovx <nikez141@outlook.com>
2021-09-25 17:09:38 +0300

    removed

---
+++ server-data/resources/lib/clients/cl_rpc.lua
@@ -0,0 +1,85 @@
+local Promises, Functions, CallIdentifier = {}, {}, GetCurrentResourceName()
+
+RPC = {}
+
+------------------------------------------------------------------
+--                  (Receive Remote Calls)                      --
+------------------------------------------------------------------
+
+
+function RPC.execute(type, event, params)
+    local callID, solved = CallIdentifier, false
+
+    local p = promise.new()
+
+    if (params == nil) then
+        params = {}
+    end
+
+    if type == 'client' then
+        print("RPC => "..type.." side => callID: "..callID)
+
+        TriggerEvent(event, params)
+        RPC.log(type, event, params, callID)
+
+    elseif type == 'server' then
+        print("RPC => "..type.." side => callID: "..callID)
+
+        TriggerServerEvent(event, params, callID)
+        RPC.log(type, event, params, callID)
+    end
+end
+
+function RPC.executeLatent(name, timeout, ...)
+    local callID, solved = CallIdentifier, false
+
+    Promises[callID] = callID
+
+    Citizen.SetTimeout(timeout, function()
+        if (solved == false) then
+            print('debug '..timeout)
+        end
+    end)
+end
+
+function RPC.log(type, event, params, callID)
+    TriggerServerEvent('log', type, event, params, callID)
+end
+
+------------------------------------------------------------------
+--                          (Exports)                           --
+------------------------------------------------------------------
+
+exports('RPC.execute', type, event, params)
+
+exports('RPC.executeLatent', name, timeout)
+
+exports('RPC.log', type, event, params)
+
+--- {TEST}
+
+RegisterCommand('tmax', function(type, event, params)
+    RPC.execute('server', 'tmaxserver', '125$')
+end)
+
+RegisterCommand('regev', function(event, funct)
+    RPC.execute('client', 'tmax1', '940$')
+end)
+
+RegisterCommand('timeout', function(name, timeout)
+    RPC.executeLatent('tmax', 5000)
+end)
+
+------------------------------------------------------------------
+--                          (Events)                           --
+------------------------------------------------------------------
+
+RegisterNetEvent('tmax1')
+AddEventHandler('tmax1', function(money)
+    print('RPC.execute => | Event: tmax1 | Function: '..money)
+end)
+
+RegisterNetEvent('tmax2')
+AddEventHandler('tmax2', function(money)
+    print('RPC.execute => | Event: tmax2 | Function: '..money)
+end)
