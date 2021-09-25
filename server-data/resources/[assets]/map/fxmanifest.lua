efe1d6f9c4bb1f4a564da261962c389f1b1235b9
stoyanovx <nikez141@outlook.com>
2021-09-25 17:20:03 +0300

    master

---
+++ server-data/resources/[assets]/map/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+description 'Circle Map with Atlas Stream'
+game 'gta5'
+
+ui_page 'html/ui.html'
+
+files {
+    'html/ui.html',
+    'html/script.js',
+    'html/style.css',
+}
+
+client_scripts {
+    'client.lua',
+}
