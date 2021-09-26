8f511fba65952bec40fe2ae477221b4498f374f2
Alex <nikez141@outlook.com>
2021-09-26 19:19:15 +0300

    master

--- server-data/resources/nikez-utils/server/sv_main.lua
+++ server-data/resources/nikez-utils/server/sv_main.lua
@@ -1,17 +1,24 @@
 RegisterNetEvent('adminCheck')
-AddEventHandler('adminCheck', function()
+AddEventHandler('adminCheck', function(admin)
+    local admin = false
     local steamid  = false
-  for k,v in pairs(GetPlayerIdentifiers(source))do        
+     for k,v in pairs(GetPlayerIdentifiers(source))do        
       if string.sub(v, 1, string.len("steam:")) == "steam:" then
         steamid = v
         print(steamid)
         
         if steamid == 'steam:110000117808edd' then
             print('Welcome Nikez... HEX: '..steamid)
+            admin = true
+            print(admin)
         elseif steamid == 'steam:1100001116cf59a' then
             print('Welcome nikigot2... HEX: '..steamid)
+            admin = true
+            print(admin)
         else
             print('not niki or alex')
+            admin = false
+            print(admin)
         end
       end
   end
